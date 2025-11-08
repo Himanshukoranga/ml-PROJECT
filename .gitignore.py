d6d981f66c12eed7ce238133c6648341ac4b9c1e
Himanshukoranga <114452114+Himanshukoranga@users.noreply.github.com>
2025-11-08 19:08:30 +0530

    Update .gitignore.py

--- .gitignore.py
+++ .gitignore.py
@@ -1,1 +1,29 @@
+# Byte-compiled / optimized / DLL files
+__pycache__/
+*.pyc
+*.pyd
+*.pyo
 
+# Virtual environment
+venv/
+env/
+.venv/
+/htmlcov/
+
+# Python interpreter artifacts
+*.egg
+*.egg-info
+.coverage
+.mypy_cache/
+.pytest_cache/
+.ipynb_checkpoints
+
+# Distribution / packaging
+/dist/
+/build/
+
+# IDE/Editor specific files (Examples)
+.vscode/
+.idea/
+*.swp
+*~
